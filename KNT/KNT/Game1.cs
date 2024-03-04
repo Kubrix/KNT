3708a805ba2f92fd1ba43ebba6cc380feae9e43a
Kubrix <33233100+Kubrix@users.noreply.github.com>
2024-03-04 15:08:10 +0300

    Update Game1.cs

--- KNT/KNT/Game1.cs
+++ KNT/KNT/Game1.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -7,64 +8,75 @@ namespace KNT;
 
 public class Game1 : Game
 {
-     Texture2D ballTexture;
-    
-     GraphicsDeviceManager _graphics;
-     SpriteBatch _spriteBatch;
-  
-ArrayList ALEnemies;
+  Texture2D ballTexture;
+  GraphicsDeviceManager _graphics;
+  SpriteBatch _spriteBatch;
+  ArrayList AlBallsX;
+  ArrayList AlBallsY;
 
-    public Game1()
-    {
-            _graphics = new GraphicsDeviceManager(this);
-    _graphics.GraphicsProfile = GraphicsProfile.HiDef;
-    Content.RootDirectory = "Content";
-    IsMouseVisible = true;
+  public Game1()
+  {
+  _graphics = new GraphicsDeviceManager(this);
+  _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+  Content.RootDirectory = "Content";
+  IsMouseVisible = true;
+  }
 
-    }
+  protected override void Initialize()
+  {
+    // TODO: Add your initialization logic here
+    AlBallsX = new ArrayList();
+    AlBallsY = new ArrayList();
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-        ALEnemies = new ArrayList();
+    AlBallsX.Add((float)100.0);
+    AlBallsY.Add((float)100.0);
 
-        _graphics.PreferredBackBufferHeight = 768;
-_graphics.PreferredBackBufferWidth = 1024;
-_graphics.ApplyChanges();
+    AlBallsX.Add((float)120.0);
+    AlBallsY.Add((float)120.0);
+    //_graphics.PreferredBackBufferHeight = 768;
+    //_graphics.PreferredBackBufferWidth = 1024;
+    //_graphics.ApplyChanges();
 
-        base.Initialize();
-    }
+    base.Initialize();
+  }
 
-    protected override void LoadContent()
-    {
-   // Create a new SpriteBatch, which can be used to draw textures.
-    _spriteBatch = new SpriteBatch(GraphicsDevice);
+  protected override void LoadContent()
+  {
+  // Create a new SpriteBatch, which can be used to draw textures.
+  _spriteBatch = new SpriteBatch(GraphicsDevice);
+  ballTexture  = Content.Load<Texture2D>("ball");
+  }
 
-    // TODO: use this.Content to load your game content here
+  protected override void Update(GameTime gameTime)
+  {
+  float val;
 
-    ballTexture  = Content.Load<Texture2D>("ball");
+  for (int i = 0;i<=AlBallsX.Count - 1; i=i+1)
+  {
+    val = (float)AlBallsX[i];
+    val = (float)(val - 1.5);
+    AlBallsX[i] = val;
 
-    }
+    val =(float) AlBallsY[i];
+    val = (float)(val - 1.5);
+    AlBallsY[i] = val;
+  }
+  base.Update(gameTime);
+  }
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        // TODO: Add your update logic here
-
-        base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
+  protected override void Draw(GameTime gameTime)
+  {
     _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
     // TODO: Add your drawing code here
     _spriteBatch.Begin();
-    _spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
-    _spriteBatch.End();
 
-    base.Draw(gameTime);
+    for (int i = 0;i<=AlBallsX.Count - 1; i=i+1)
+    {
+      _spriteBatch.Draw(ballTexture, new Vector2((float)AlBallsX[i],(float)AlBallsY[i]), Color.White);
     }
+
+    _spriteBatch.End();
+    base.Draw(gameTime);
+  }
 }
